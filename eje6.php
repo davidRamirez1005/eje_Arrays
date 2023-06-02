1a640175d2f318c742a619377ccf0281dbe2ed7e
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-02 16:49:16 -0500

    feat: :sparkles: terminacion eje7
    completado ejercicio 7 funcional

---
+++ eje6.php
@@ -0,0 +1,14 @@
+<?php
+
+function busqueda_planeta($planetas,$busqueda){
+    if (in_array($busqueda, $planetas)) {
+        echo "EXISTE EL PLANETA :)";
+    }
+    else{
+        echo "NO EXISTE EL PLANETA :(";
+    }
+}
+$planetas =["sol","mercurio","venus","tierra","marte","jupiter","saturno","urano","neptuno","pluton"];
+$busqueda = $_POST["busqueda"];
+busqueda_planeta($planetas,strtolower($busqueda))
+?>
