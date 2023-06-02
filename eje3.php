493945dba90f3f2d506644ec88a812d71ddb21e1
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-02 17:12:41 -0500

    feat: :white_check_mark: prueba
    prueba de funcionamiento

--- eje3.php
+++ eje3.php
@@ -1,7 +1,8 @@
 <?php
 function universo(){
     $planetas = ["planeta1"=>true,"planeta2"=>false,"planeta3"=>true,"planeta4"=>false,"planeta5"=>false,"planeta6"=>true];
-    print_r(array_filter($planetas));
+    $a=(array_filter($planetas));
+    print_r($a);
 };
 universo()
 ?>
