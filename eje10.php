e6b80e56d22f2463c755f741fa1e04569f49e63f
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-02 20:35:02 -0500

    feat: :sparkles: eje10 completado

---
+++ eje10.php
@@ -0,0 +1,27 @@
+<?php
+
+$sistema_solar_1 = [
+    "Sol",
+    "Mercurio",
+    "Venus",
+    "Tierra",
+    "Marte",
+    "Júpiter",
+    "Saturno",
+    "Urano",
+    "Neptuno"
+];
+$sistema_solar_2 = [
+    "Sol",
+    "Planeta_X",
+    "Planeta_Y",
+    "Venus",
+    "Satélite_A",
+    "Tierra",
+    "Satélite_C",
+    "Satélite_D",
+    "Satélite_E"
+];
+$result = array_intersect($sistema_solar_1, $sistema_solar_2);
+print_r($result);
+?>
