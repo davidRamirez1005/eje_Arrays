7ab0b29149b3c223a54f518a6d1fa4974eb938e4
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-06-02 20:35:45 -0500

    feat: :sparkles: eje13 completado
    funcional

---
+++ eje13.php
@@ -0,0 +1,19 @@
+<?php
+
+$sistema_solar_1 = [
+    "Sol",
+    "Mercurio",
+    "Venus",
+    "Tierra",
+    "Marte",
+    "Júpiter",
+    "Saturno",
+    "Urano",
+    "Neptuno"
+];
+
+$reversed = array_reverse($sistema_solar_1);
+
+print_r($reversed);
+
+?>
